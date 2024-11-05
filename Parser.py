c411d86b7bc1be8d84326a875fb3db2bd976ac9b
Viatkin Dmytro <viatkindima@gmail.com>
2024-11-05 15:39:46 +0200

    New bot

--- Parser.py
+++ Parser.py
@@ -1,10 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def parser(url):
 
     r = requests.get(url)
-    soup = BeautifulSoup(r.text,'html.parser')
-    anekdots = soup.find_all('div', class_ = "text",)
+    soup = BeautifulSoup(r.text, "html.parser")
+    anekdots = soup.find_all(
+        "div",
+        class_="text",
+    )
 
     return [c.text for c in anekdots]
---
+++ bot.py
@@ -0,0 +1,36 @@
+import asyncio
+import random
+from config import key
+from aiogram import Bot, Dispatcher
+from Parser import parser
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+
+url = "https://www.anekdot.ru/release/anekdot/week/"
+list_jokes = parser(url)
+
+bot = Bot(token=key)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer('Hello')
+
+
+async def main():
+    await dp.start_polling(bot)
+'''
+@dp.message_handler(commands=["start"])
+async def send_welcome(message: types.Message):
+    await message.reply("Hello!\n I am best Bot \n Send my message")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+'''
+
+if __name__ == "__main__":
+    asyncio.run(main())
