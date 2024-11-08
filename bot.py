698baa9a25f9665dab7243b17813ef38fc726b44
Viatkin Dmytro <viatkindima@gmail.com>
2024-11-08 18:21:00 +0200

    env

--- bot.py
+++ bot.py
@@ -1,18 +1,22 @@
-
+import os
 import asyncio
-from config import key, url
+from config import url
 from aiogram import Bot, Dispatcher
 from Parser import parser
 from app.handlers import router
+from dotenv import load_dotenv
 
 
 list_jokes = parser(url)
 
-bot = Bot(token=key)
-dp = Dispatcher()
+
+
 
 
 async def main():
+    load_dotenv()
+    bot = Bot(token=os.getenv('TOKEN'))
+    dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
 
--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-key = '8088656093:AAHKaOrV6a-MdIky7vg4FnTYIkaF-BJdmsw'
+
 url = "https://www.anekdot.ru/release/anekdot/week/"
 cars = ['Tesla', 'Mercedes', 'BMW', 'ZAZ']
